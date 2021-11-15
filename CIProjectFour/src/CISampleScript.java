7530230f977740493b4ba59dbfc9e87e77001ef1
sathishsuriya <sathishsuriya@gmail.com>
2021-11-15 19:02:54 +0530

    selenium version

---
+++ CIProjectFour/src/CISampleScript.java
@@ -0,0 +1,16 @@
+
+public class CISampleScript {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("selenium");
+		
+		System.out.println("oracle");
+		
+		System.out.println("tosca");
+		
+		System.out.println("uft");
+	}
+
+}
